b8bd028442f1941e4086ce29994fdc75b46d7c41
Jimmy Shen <thejimmyshen@gmail.com>
2016-11-25 21:08:12 -0500

    Fix broken bluetooth service handler errors. Style cleanup.

--- server/blackduck/btservice.py
+++ server/blackduck/btservice.py
@@ -21,37 +21,38 @@ class BluetoothService(Thread):
         self.screen_manager = screen_manager
         self.handlers = {
             'list_tasks': self._handle_list_tasks,
-            'get_icon': self._handle_get_icon,
+            'list_updated_tasks': self._handle_list_updated_tasks,
+            'batchget_icons': self._handle_batchget_icons,
             'activate_task': self._handle_activate_task,
         }
 
-    def make_response(self, status, **kwargs):
+    def _make_response(self, status, **kwargs):
         response = {'status': status}
         response.update(kwargs)
         return response
 
-    def make_client_error_response(self, msg, *fmtargs):
+    def _make_client_error_response(self, msg, *fmtargs):
         if fmtargs:
             msg = msg % (*fmtargs)
 
         log.error(msg)
-        return make_response('client-error', error=msg)
+        return _make_response('client-error', error=msg)
 
-    def make_server_error_response(self, exc, msg, *fmtargs):
+    def _make_server_error_response(self, exc, msg, *fmtargs):
         if fmtargs:
             msg = msg % (*fmtargs)
 
         log.exception(msg, exc_info=exc)
-        return make_response('server-error', error=': '.join([msg, exc.message]))
+        return _make_response('server-error', error=': '.join([msg, exc.message]))
 
-    def make_ok_response(self, payload=None):
-        return make_response('ok', data=payload or {})
+    def _make_ok_response(self, payload=None):
+        return _make_response('ok', data=payload or {})
 
     def handle_message(self, msg):
         if 'command' not in msg:
-            return make_client_error_response('Missing "command" in message.')
+            return _make_client_error_response('Missing "command" in message.')
         elif msg['command'] not in handlers:
-            return make_client_error_response('Received unrecognized command "%s"', msg['command'])
+            return _make_client_error_response('Received unrecognized command "%s"', msg['command'])
         else:
             payload = msg.get('payload', {})
             handler = self.dispatch_table[msg['command_name']]
@@ -59,13 +60,13 @@ class BluetoothService(Thread):
 
     def _handle_list_tasks(self, payload):
         try:
-            return make_ok_response({'tasks': self.screen_manager.list_tasks()})
+            return _make_ok_response({'tasks': self.screen_manager.list_tasks()})
         except Exception as e:
-            return make_server_error_response(e, 'Could not list tasks')
+            return _make_server_error_response(e, 'Could not list tasks')
 
     def _handle_list_task_updates(self, payload):
         if 'last_update_ts' not in payload:
-            return make_client_error_response('Missing "last_update_ts" in payload.')
+            return _make_client_error_response('Missing "last_update_ts" in payload.')
 
         try:
             client_ts = long(payload['last_update_ts'])
@@ -74,34 +75,34 @@ class BluetoothService(Thread):
                 if task['last_update_ts'] > client_ts:
                     tasks.append(task)
 
-            return make_ok_response({'tasks': tasks})
+            return _make_ok_response({'tasks': tasks})
         except Exception as e:
-            return make_server_error_response(e, 'Could not get task updates since %s', payload['last_update_ts'])
+            return _make_server_error_response(e, 'Could not get task updates since %s', payload['last_update_ts'])
 
-    def _handle_batchget_icon(self, payload):
+    def _handle_batchget_icons(self, payload):
         if 'icon_ids' not in payload:
-            return make_client_error_response('Missing "icon_ids" in payload.')
+            return _make_client_error_response('Missing "icon_ids" in payload.')
 
         try:
             icons = {icon_id: self.screen_manager.get_icon(icon_id) for icon_id in payload['icon_ids']}
-            return make_ok_response({'icons': icons})
+            return _make_ok_response({'icons': icons})
         except Exception as e:
-            return make_server_error_response(e, 'Could not get icon with ID %s', payload['icon_id'])
+            return _make_server_error_response(e, 'Could not get icon with ID %s', payload['icon_id'])
 
     def _handle_activate_task(self, payload):
         if 'task_id' not in payload:
-            return make_client_error_response('Missing "task_id" in payload.')
+            return _make_client_error_response('Missing "task_id" in payload.')
 
         try:
             task_id = payload['task_id']
             self.screen_manager.activate_task(task_id)
-            return make_ok_response()
+            return _make_ok_response()
         except Exception as e:
-            return make_server_error_response(e, 'Could not activate task with ID %s', payload['task_id'])
+            return _make_server_error_response(e, 'Could not activate task with ID %s', payload['task_id'])
 
     def manage_connection(self, client_sock, client_addr):
         try:
-            # Duckypatch in a read() method to make msgpack
+            # Duckypatch in a read() method to _make msgpack
             # stream deserializer happy...
             client_sock.read = lambda bytelen: client_sock.recv(bytelen)
 
@@ -128,14 +129,14 @@ class BluetoothService(Thread):
 
         try:
             while True:
-                self.trace('INFO', 'Waiting for connection from client...')
+                log.info('Waiting for connection from client...')
                 client_sock, client_addr = sock.accept()
                 manage_connection(client_sock, client_addr)
         except KeyboardInterrupt:
-            self.trace('INFO', '\b\bUser requested termination.')
+            log.info('User requested termination.')
         finally:
             sock.close()
             bluetooth.stop_advertising(sock)
-            self.trace('INFO', 'Bluetooth service stopped.')
+            log.info('Bluetooth service stopped.')
 
 
