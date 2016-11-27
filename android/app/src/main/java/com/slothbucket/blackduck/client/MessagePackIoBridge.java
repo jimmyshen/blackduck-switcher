b453058577e0a8b4b2d67d62c6cb2e07b5521edb
Jimmy Shen <thejimmyshen@gmail.com>
2016-11-27 15:06:34 -0500

    Fix stream being auto-closed (THAT was fun).

--- android/app/src/main/java/com/slothbucket/blackduck/client/MessagePackIoBridge.java
+++ android/app/src/main/java/com/slothbucket/blackduck/client/MessagePackIoBridge.java
@@ -1,5 +1,8 @@
 package com.slothbucket.blackduck.client;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.msgpack.jackson.dataformat.MessagePackFactory;
@@ -9,10 +12,15 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 class MessagePackIoBridge implements MessageIoBridge {
-    private final ObjectMapper mapper = new ObjectMapper(new MessagePackFactory());
+    // Disable the default Jackson configuration that closes my sockets!!! >_<;
+    private final JsonFactory jsonFactory =
+        new MessagePackFactory()
+            .disable(JsonParser.Feature.AUTO_CLOSE_SOURCE)
+            .disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
+    private final ObjectMapper mapper = new ObjectMapper(jsonFactory);
 
     @Override
-    public void write(ServiceRequest request, OutputStream outputStream) throws IOException {
+    public void write(ServiceRequest request, final OutputStream outputStream) throws IOException {
         mapper.writerFor(ServiceRequest.class).writeValue(outputStream, request);
     }
 
