f5e3a10cf4ba820837fed6050f4b2c2b36333288
Jimmy Shen <thejimmyshen@gmail.com>
2016-12-01 03:17:40 -0500

    track window title changes

--- server/blackduck/screen.py
+++ server/blackduck/screen.py
@@ -16,6 +16,7 @@ class ScreenManager(object):
         self.tasks = {}
         self.icons = {}
         self.windows_by_task_id = {}
+        self.window_handler_ids = {}
 
     def _encode_pixels(self, pixels):
         return base64.standard_b64encode(pixels.encode('zlib_codec'))
@@ -52,6 +53,18 @@ class ScreenManager(object):
     def _is_eligible_window(self, window):
         return window.get_window_type() == Wnck.WindowType.NORMAL
 
+    def _on_window_name_changed(self, window, *args):
+        log.debug('Received "name-changed" for window %s (%s)', window.get_xid(), window.get_name())
+        task = self._window_to_task(window, long(time()))
+        self.tasks[task['id']] = task
+
+    def _connect_window_signals(self, window):
+        window_id = window.get_xid()
+        if window_id not in self.window_handler_ids:
+            handler_id = window.connect('name-changed', self._on_window_name_changed)
+            if handler_id > 0:
+                self.window_handler_ids[window_id] = handler_id
+
     def _on_window_open(self, screen, window):
         if not self._is_eligible_window(window):
             return
@@ -61,20 +74,27 @@ class ScreenManager(object):
         task = self._window_to_task(window, long(time()))
         self.tasks[task['id']] = task
         self.windows_by_task_id[task['id']] = window
+        self._connect_window_signals(window)
 
     def _on_window_close(self, screen, window):
         if not self._is_eligible_window(window):
             return
 
-        log.debug('Window %d closed.', window.get_xid())
+        window_id = window.get_xid()
+        log.debug('Window %d closed.', window_id)
 
-        task_id = str(window.get_xid())
+        task_id = str(window_id)
         if task_id in self.tasks:
             task = self.tasks[task_id]
             task['is_open'] = False
             task['last_update_ts'] = long(time())
 
         if task_id in self.windows_by_task_id:
+            window = self.windows_by_task_id[task_id]
+            if window_id in self.window_handler_ids:
+                window.disconnect(self.window_handler_ids[window_id])
+                del self.window_handler_ids[window_id]
+
             del self.windows_by_task_id[task_id]
 
     def _ensure_initialized(self):
@@ -93,6 +113,7 @@ class ScreenManager(object):
             task = self._window_to_task(window, seed_ts)
             self.tasks[task['id']] = task
             self.windows_by_task_id[task['id']] = window
+            self._connect_window_signals(window)
 
         log.info('ScreenManager initialized with %d tasks and %d icons.',
             len(self.tasks), len(self.icons))
